86cb91c775596ea095efa5604a169f82a6ea79ab
Ines Chami <ineschami@hotmail.com>
2019-08-13 11:43:19 -0700

    bugfix

--- codes/models/__init__.py
+++ codes/models/__init__.py
@@ -1,5 +1,5 @@
-from euc_models import EKGEModel
-from hyp_models import HKGEModel
+from .euc_models import EKGEModel
+from .hyp_models import HKGEModel
 
 # Do not forget to modify this line when you add a new model in the "forward" function
 EUC_MODELS = ['TransE', 'DistMult', 'ComplEx', 'RotatE', 'pRotatE', 'ReflectionE', 'RotationE']
---
+++ codes/models/base.py
@@ -0,0 +1,228 @@
+#!/usr/bin/python3
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import logging
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from sklearn.metrics import average_precision_score
+
+from torch.utils.data import DataLoader
+
+from dataloader import TestDataset
+
+
+class KGEModel(nn.Module):
+    def __init__(self, model_name, nentity, nrelation, hidden_dim, gamma, p_norm, dropout):
+        super(KGEModel, self).__init__()
+        self.model_name = model_name
+        self.nentity = nentity
+        self.nrelation = nrelation
+        self.hidden_dim = hidden_dim
+        self.epsilon = 2.0
+        self.dropout = dropout
+        self.p_norm = p_norm
+
+        self.gamma = nn.Parameter(
+            torch.Tensor([gamma]),
+            requires_grad=False
+        )
+
+    def forward(self, sample, mode='single'):
+        '''
+        Forward function that calculate the score of a batch of triples.
+        In the 'single' mode, sample is a batch of triple.
+        In the 'head-batch' or 'tail-batch' mode, sample consists two part.
+        The first part is usually the positive sample.
+        And the second part is the entities in the negative samples.
+        Because negative samples and positive samples usually share two elements
+        in their triple ((head, relation) or (relation, tail)).
+        '''
+        raise NotImplementedError
+
+    @staticmethod
+    def train_step(model, optimizer, train_iterator, args):
+        '''
+        A single train step. Apply back-propation and return the loss
+        '''
+
+        model.train()
+
+        optimizer.zero_grad()
+
+        positive_sample, negative_sample, subsampling_weight, mode = next(train_iterator)
+
+        if args.cuda:
+            positive_sample = positive_sample.cuda()
+            negative_sample = negative_sample.cuda()
+            subsampling_weight = subsampling_weight.cuda()
+
+        negative_score = model((positive_sample, negative_sample), mode=mode)
+
+        if args.negative_adversarial_sampling:
+            # In self-adversarial sampling, we do not apply back-propagation on the sampling weight
+            negative_score = (F.softmax(negative_score * args.adversarial_temperature, dim=1).detach()
+                              * F.logsigmoid(-negative_score)).sum(dim=1)
+        else:
+            negative_score = F.logsigmoid(-negative_score).mean(dim=1)
+
+        positive_score = model(positive_sample)
+
+        positive_score = F.logsigmoid(positive_score).squeeze(dim=1)
+
+        if args.uni_weight:
+            positive_sample_loss = - positive_score.mean()
+            negative_sample_loss = - negative_score.mean()
+        else:
+            positive_sample_loss = - (subsampling_weight * positive_score).sum() / subsampling_weight.sum()
+            negative_sample_loss = - (subsampling_weight * negative_score).sum() / subsampling_weight.sum()
+
+        loss = (positive_sample_loss + negative_sample_loss) / 2
+
+        if args.regularization != 0.0:
+            # Use L3 regularization for ComplEx and DistMult
+            regularization = args.regularization * (
+                    model.entity_embedding.norm(p=3) ** 3 +
+                    model.relation_embedding.norm(p=3).norm(p=3) ** 3
+            )
+            loss = loss + regularization
+            regularization_log = {'regularization': regularization.item()}
+        else:
+            regularization_log = {}
+
+        loss.backward()
+
+        optimizer.step()
+
+        log = {
+            **regularization_log,
+            'positive_sample_loss': positive_sample_loss.item(),
+            'negative_sample_loss': negative_sample_loss.item(),
+            'loss': loss.item()
+        }
+
+        return log
+
+    @staticmethod
+    def test_step(model, test_triples, all_true_triples, args):
+        '''
+        Evaluate the model on test or valid datasets
+        '''
+
+        model.eval()
+
+        if args.countries:
+            # Countries S* datasets are evaluated on AUC-PR
+            # Process test data for AUC-PR evaluation
+            sample = list()
+            y_true = list()
+            for head, relation, tail in test_triples:
+                for candidate_region in args.regions:
+                    y_true.append(1 if candidate_region == tail else 0)
+                    sample.append((head, relation, candidate_region))
+
+            sample = torch.LongTensor(sample)
+            if args.cuda:
+                sample = sample.cuda()
+
+            with torch.no_grad():
+                y_score = model(sample).squeeze(1).cpu().numpy()
+
+            y_true = np.array(y_true)
+
+            # average_precision_score is the same as auc_pr
+            auc_pr = average_precision_score(y_true, y_score)
+
+            metrics = {'auc_pr': auc_pr}
+
+        else:
+            # Otherwise use standard (filtered) MRR, MR, HITS@1, HITS@3, and HITS@10 metrics
+            # Prepare dataloader for evaluation
+            test_dataloader_head = DataLoader(
+                TestDataset(
+                    test_triples,
+                    all_true_triples,
+                    args.nentity,
+                    args.nrelation,
+                    'head-batch'
+                ),
+                batch_size=args.test_batch_size,
+                num_workers=max(1, args.cpu_num // 2),
+                collate_fn=TestDataset.collate_fn
+            )
+
+            test_dataloader_tail = DataLoader(
+                TestDataset(
+                    test_triples,
+                    all_true_triples,
+                    args.nentity,
+                    args.nrelation,
+                    'tail-batch'
+                ),
+                batch_size=args.test_batch_size,
+                num_workers=max(1, args.cpu_num // 2),
+                collate_fn=TestDataset.collate_fn
+            )
+
+            test_dataset_list = [test_dataloader_head, test_dataloader_tail]
+
+            logs = []
+
+            step = 0
+            total_steps = sum([len(dataset) for dataset in test_dataset_list])
+
+            with torch.no_grad():
+                for test_dataset in test_dataset_list:
+                    for positive_sample, negative_sample, filter_bias, mode in test_dataset:
+                        if args.cuda:
+                            positive_sample = positive_sample.cuda()
+                            negative_sample = negative_sample.cuda()
+                            filter_bias = filter_bias.cuda()
+
+                        batch_size = positive_sample.size(0)
+
+                        score = model((positive_sample, negative_sample), mode)
+                        score += filter_bias
+
+                        # Explicitly sort all the entities to ensure that there is no test exposure bias
+                        argsort = torch.argsort(score, dim=1, descending=True)
+
+                        if mode == 'head-batch':
+                            positive_arg = positive_sample[:, 0]
+                        elif mode == 'tail-batch':
+                            positive_arg = positive_sample[:, 2]
+                        else:
+                            raise ValueError('mode %s not supported' % mode)
+
+                        for i in range(batch_size):
+                            # Notice that argsort is not ranking
+                            ranking = (argsort[i, :] == positive_arg[i]).nonzero()
+                            assert ranking.size(0) == 1
+
+                            # ranking + 1 is the true ranking used in evaluation metrics
+                            ranking = 1 + ranking.item()
+                            logs.append({
+                                'MRR': 1.0 / ranking,
+                                'MR': float(ranking),
+                                'HITS@1': 1.0 if ranking <= 1 else 0.0,
+                                'HITS@3': 1.0 if ranking <= 3 else 0.0,
+                                'HITS@10': 1.0 if ranking <= 10 else 0.0,
+                            })
+
+                        if step % args.test_log_steps == 0:
+                            logging.info('Evaluating the model... (%d/%d)' % (step, total_steps))
+
+                        step += 1
+
+            metrics = {}
+            for metric in logs[0].keys():
+                metrics[metric] = sum([log[metric] for log in logs]) / len(logs)
+
+        return metrics
